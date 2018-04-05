e5b0a9abee502e93eaae923ed6bab139916a0ec1
huilent <huilentorres@gmail.com>
2018-04-05 16:47:41 -0300

    Función 2 y 3

--- TP1/función 2.cpp
+++ TP1/función 2.cpp
@@ -2,3 +2,8 @@
 // Created by icm-01 on 05/04/18.
 //
 
+#include <iostream>
+#include <string>
+
+using namespace std;
+
--- TP1/main.cpp
+++ TP1/main.cpp
@@ -10,13 +10,52 @@ bool esPrimo(int n) {
     return n == i;
 }
 
-int main() {
+/*int main() {
     int resultado = esPrimo(1);
     if (resultado) {
         cout << "Es primo";
     } else {
         cout << "No es primo";
     }
+    return 0;
+}*/
+int cantidadPrimosManoresOIguales (int n) {
+    int i = 2;
+    int j = 0;
+    while (i <= n) {
+        if (esPrimo(i)){
+            j = j+1;
+        }
+        i = i+1;
+    }
+    return j;
+}
+
+/*int main() {
+    int resultado = cantidadPrimosManoresOIguales(11);
+    cout << resultado;
+    return 0;
+}
+*/
+int cantidadDivisoresPrimos (int n) {
+    int i = 0;
+    int j = 2;
+    if (esPrimo(n)){
+        i = 1;
+    } else {
+        while (j <= n){
+            if (esPrimo(j) && n % j == 0) {
+                i = i + 1;
+            }
+        j = j + 1;
+        }
+    }
+    return i;
+}
+
+int main() {
+    int resultado = cantidadDivisoresPrimos(9);
+    cout << resultado;
     return 0;
 }
 
